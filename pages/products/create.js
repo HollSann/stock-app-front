072b978f8815655bfe242a8c01ddaa2e85a29cb6
Hollsann <Santiago_hollmann@outlook.com>
2022-06-19 13:33:10 -0300

    Carga del frontend

---
+++ pages/products/create.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+const initialState = { name: '', price: 0 }
+function Create() {
+
+  const [product, setProduct] = useState(initialState) //inicializa con un objeto vacio
+  const handleChange = (e) => {
+
+    const inputValue = e.target.value
+    const inputName = e.target.name
+    setProduct({
+      ...product,
+      [inputName]: inputValue
+    })
+
+  }
+  const handleClick = (e) => {
+    (e).preventDefault()
+    fetch('http://localhost:5000/api/v1/products', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(product),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setProduct(initialState)
+        console.log('Producto creado exitosamente')
+      })
+      .catch((err) => {
+        console.log({ err })
+      })
+  }
+
+
+  return (
+    <>
+      <div>
+        <h1>Crear nuevo producto</h1>
+        <form>
+          <input
+            type='text'
+            name='name'
+            placeholder='Nombre del producto'
+            onChange={handleChange}
+            value={product.name}
+          />
+
+
+          <input
+            type='number'
+            name='price'
+            placeholder='Precio del producto'
+            onChange={handleChange}
+            value={product.price}
+          />
+
+
+          <button onClick={handleClick}>Crear producto</button>
+        </form>
+
+
+      </div>
+      <style jsx>
+        {
+          `
+          form{
+            display:flex;
+            flex-direction:column;
+            width:20rem;
+            margin:0 auto;
+          }
+          input{
+            margin-bottom:.5rem;
+          }
+          h1{
+            text-align:center;
+          }
+          `
+        }
+      </style>
+    </>
+  );
+}
+
+export default Create;
